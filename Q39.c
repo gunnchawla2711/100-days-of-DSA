cc4679bc22d91152f88b73b039e0615b344c86df
gunnchawla2711 <gunnchawla2711@gmail.com>
2026-03-18 19:04:53 +0530

    Create Q39.c

---
+++ Q39.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int heap[MAX];
+int size = 0;
+
+// Swap
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Heapify Up (Insert)
+void heapifyUp(int i) {
+    while (i > 0 && heap[(i - 1) / 2] > heap[i]) {
+        swap(&heap[i], &heap[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+}
+
+// Heapify Down (Delete)
+void heapifyDown(int i) {
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && heap[left] < heap[smallest])
+        smallest = left;
+
+    if (right < size && heap[right] < heap[smallest])
+        smallest = right;
+
+    if (smallest != i) {
+        swap(&heap[i], &heap[smallest]);
+        heapifyDown(smallest);
+    }
+}
+
+// Insert
+void insert(int x) {
+    heap[size] = x;
+    heapifyUp(size);
+    size++;
+}
+
+// Extract Min
+int extractMin() {
+    if (size == 0) return -1;
+
+    int min = heap[0];
+    heap[0] = heap[size - 1];
+    size--;
+
+    heapifyDown(0);
+    return min;
+}
+
+// Peek
+int peek() {
+    if (size == 0) return -1;
+    return heap[0];
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    while (N--) {
+        char op[20];
+        scanf("%s", op);
+
+        if (strcmp(op, "insert") == 0) {
+            int x;
+            scanf("%d", &x);
+            insert(x);
+        }
+        else if (strcmp(op, "extractMin") == 0) {
+            printf("%d\n", extractMin());
+        }
+        else if (strcmp(op, "peek") == 0) {
+            printf("%d\n", peek());
+        }
+    }
+
+    return 0;
+}
